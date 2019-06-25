d4b501f3d4d01899133e8f16f4997e51332e75ae
cliffordjorgensen <cliffordjorgensen@gmail.com>
2019-06-25 16:49:13 -0700

    started with basic outline from great bay class activity

---
+++ SQL/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS bamazon_DB;
+CREATE DATABASE bamazon_DB;
+
+USE bamazon_DB;
+
+CREATE TABLE products (
+  item_id INT NOT NULL,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(100) NULL,
+  price INT NULL,
+  stock_quantity DECIMAL(10,4) NULL,
+  PRIMARY KEY (item_id)
+);
+
+SELECT * FROM products;
+
